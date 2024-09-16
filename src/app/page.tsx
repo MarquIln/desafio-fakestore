d6cf94b8de32422c74448fe6e5b70d46e88c8e0e
Marcos <marcosraach.1@gmail.com>
2024-09-16 16:58:08 -0300

    fix: spinner while content is not available

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,38 @@
 'use client'
 
 import { useCartStore } from '@/context/cart-store'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import AllProductsPage from './products/page'
+import { Spinner } from '@/components/spinner'
 
 export default function Home() {
   const loadCartFromLocalStorage = useCartStore(
     (state) => state.loadCartFromLocalStorage,
   )
+  const [isPageLoading, setIsPageLoading] = useState(true)
 
   useEffect(() => {
     loadCartFromLocalStorage()
+
+    setTimeout(() => {
+      setIsPageLoading(false)
+    }, 1000)
   }, [loadCartFromLocalStorage])
+
+  if (isPageLoading) {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+        }}
+      >
+        <Spinner />
+      </div>
+    )
+  }
 
   return <AllProductsPage />
 }
