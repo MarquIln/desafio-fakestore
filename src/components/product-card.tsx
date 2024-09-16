964f06bd4e1528c839227df791713d699e934cc1
Marcos <marcosraach.1@gmail.com>
2024-09-16 17:00:11 -0300

    fix: changed productGrid to same file as productCard

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -3,3 +3,10 @@ import styled from 'styled-components'
 export const ProductCard = styled.div`
   position: relative;
 `
+
+export const ProductGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  gap: 20px;
+  margin-top: 20px;
+`
--- src/components/product-grid.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const ProductGrid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-  gap: 20px;
-  margin-top: 20px;
-`
