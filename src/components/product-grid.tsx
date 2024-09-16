c301a88977d1056f22b18121085bbd8a98731cb1
Marcos <marcosraach.1@gmail.com>
2024-09-16 14:50:56 -0300

    feat: componentization page.tsx

---
+++ src/components/product-grid.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const ProductGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  gap: 20px;
+  margin-top: 20px;
+`
